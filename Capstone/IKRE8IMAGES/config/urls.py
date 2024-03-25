de49d49a77e422bc5fd2ab6e367382635c06314e
MGray2 <mgray24@basecampcodingacademy.org>
2024-03-25 14:34:45 -0500

    Added static and template files

--- Capstone/IKRE8IMAGES/config/urls.py
+++ Capstone/IKRE8IMAGES/config/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from app.views import Home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("home/", Home)
-    path("appointment/", include('appointment.urls'))
+    path("home/", Home),
 ]
